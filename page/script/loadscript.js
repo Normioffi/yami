f5cbd2817ae76fc38cb52f9874cdf453ff7236b8
Normi offi <88531106+Normioffi@users.noreply.github.com>
2021-12-10 11:41:41 +0100

    Création loadscript.js

---
+++ page/script/loadscript.js
@@ -0,0 +1,24 @@
+const cardbar = document.querySelector('div[class="card"]');
+const navbar = document.querySelector('div[class="nav"]');
+navbar.style.display = "none";
+cardbar.style.display = "none";
+
+function onReady(callback) {
+  var intervalId = window.setInterval(function() {
+    if (document.getElementsByTagName('body')[0] !== undefined) {
+      window.clearInterval(intervalId);
+      callback.call(this);
+    
+    }
+  }, 3000);
+}
+
+function setVisible(selector, visible) {
+  document.querySelector(selector).style.display = visible ? 'block' : 'none';
+}
+
+onReady(function() {
+  setVisible('.center', false);
+  navbar.style.display = "block";
+  cardbar.style.display = "block";
+});
