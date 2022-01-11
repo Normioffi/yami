4480881711b6d980469562ba52b20c969fd8c097
Normi offi <88531106+Normioffi@users.noreply.github.com>
2022-01-11 07:22:41 +0100

    Mise à jour ping.js
    Suppression de quelques demande et modification de la réponse d'interaction

--- slashcmd/ping.js
+++ slashcmd/ping.js
@@ -1,17 +1,12 @@
-const { SlashCommandBuilder } = require('@discordjs/builders');
-const { MessageEmbed } = require('discord.js');
-const wait = require('util').promisify(setTimeout);
+const { SlashCommandBuilder } = require("@discordjs/builders");
 
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('ping')
-		.setDescription('Renvoie Pong!'),
-	async execute(interaction) {
+  data: new SlashCommandBuilder()
+    .setName("ping")
+    .setDescription("Obtient mon ping, et je te le renvoie en pong!"),
+  async execute(interaction) {
 
-		const embed = new MessageEmbed()
-			.setColor('#0099ff')
-			.setTitle('Pong!')
-			.setDescription('Tu veux faire une partie :eyes:?');
-    
-	},
+    await interaction.reply({ content: `Ping pong: ${interaction.client.ws.ping}ms` });
+
+  },
 };
