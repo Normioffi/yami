3c36303a899efbec9674f44d65c2c12d6a8df7a1
Normi offi <88531106+Normioffi@users.noreply.github.com>
2022-01-11 06:58:49 +0100

    Mise a jour Ping.js
    Suppression du menu de sélection + son interaction

--- slashcmd/ping.js
+++ slashcmd/ping.js
@@ -1,5 +1,5 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
-const { MessageEmbed, MessageSelectMenu, MessageActionRow } = require('discord.js');
+const { MessageEmbed } = require('discord.js');
 const wait = require('util').promisify(setTimeout);
 
 module.exports = {
@@ -13,37 +13,5 @@ module.exports = {
 			.setTitle('Pong!')
 			.setDescription('Tu veux faire une partie :eyes:?');
     
-    const row = new MessageActionRow()
-			.addComponents(
-				new MessageSelectMenu()
-					.setCustomId('select')
-					.setPlaceholder('Choisi ici!')
-					.setMinValues(1)
-					.setMaxValues(1)
-					.addOptions([
-						{
-							label: 'Oui',
-							description: 'Faire une partie de ping pong',
-							value: 'yes_game',
-						},
-						{
-							label: 'Non',
-							description: 'Ne pas faire une partie',
-							value: 'no_game',
-						}
-					]),
-			);
-		await interaction.reply({ embeds: [embed], components: [row] });
-	
-    if (interaction.customId === 'select') {
-        let choice = interaction.values[0] 
-     if(choice === 'yes_game'){
-       await interaction.reply({ content: 'Ok, tu commence!', embeds: [] })
-        }
-    else if(choice === 'no_game'){
-       
-       await interaction.reply({ content: 'Bon bah la prochaine fois.', embeds: [] })
-    }
-	}
 	},
 };
