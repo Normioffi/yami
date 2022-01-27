7ca3573d6d552e649a2b48e6da28508a2130b8d5
Normi offi <88531106+Normioffi@users.noreply.github.com>
2022-01-27 07:42:04 +0100

    Ajout d'un pack et ajout de deux évent

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 require('/app/deploy-commands.js');
 const fs = require('fs');
 
+const moment = require('moment');
+
 const { Client, Collection, Intents } = require('discord.js');
-// Create a new client instance
+
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
 client.commands = new Collection();
@@ -11,14 +13,14 @@ const commandSlFiles = fs.readdirSync('./slashcmd').filter(file => file.endsWith
 
 for (const file of commandSlFiles) {
 	const scommand = require(`./slashcmd/${file}`);
-	// Set a new item in the Collection
-	// With the key as the command name and the value as the exported module
+  
 	client.commands.set(scommand.data.name, scommand);
 }
 
 client.once('ready', () => {
 	console.log('Ready!');
-	client.user.setActivity(`?help | ${client.guilds.cache.size} Serveur`);
+  client.user.setActivity(`?help | ${client.guilds.cache.size} Serveurs`);
+  
 });
 
 client.on('interactionCreate', async interaction => {
@@ -32,9 +34,44 @@ client.on('interactionCreate', async interaction => {
 		await command.execute(interaction);
 	} catch (error) {
 		console.error(error);
-		await interaction.reply({ content: 'Une erreur est survenue lors de l\'utilisation de la commande!', ephemeral: true });
+		await interaction.deferReply({ content: 'Une erreur est survenue lors de l\'utilisation de la commande!', ephemeral: true });
 	}
 });
 
-// Login to Discord with your client's token
+const { MessageEmbed } = require("discord.js");
+client.on("guildCreate", guild => {
+const created = moment(guild.createdAt).format('DD/MM/YY');
+  let joinEmbed = new MessageEmbed()
+  .setColor("#00C505")
+  .setTitle(`J'ai été ajouté dans ${guild.name}`)
+  .setThumbnail(guild.iconURL())
+  .addFields({name: "Identifiant", value: `${guild.id}`},
+             {name: "Nombre de membres", value: `${guild.memberCount}`},
+             {name: "Description du serveur", value: `${guild.description || "Aucune description"}`},
+             {name: "Date de création", value: `${created}`});
+  
+  
+client.channels.cache.get('930361647040720937').send({ embeds: [joinEmbed]});
+  console.log(`J'ai rejoint le serveur ${guild.name}, je suis maintenant dans ${client.guilds.cache.size} serveurs`);
+  client.user.setActivity(`?help | ${client.guilds.cache.size} Serveurs`);
+});
+
+client.on("guildDelete", guild => {
+  
+const created = moment(guild.createdAt).format('DD/MM/YY');
+  
+  let leaveEmbed = new MessageEmbed()
+  .setColor("#C50000")
+  .setTitle(`J'ai été retirer de ${guild.name}`)
+  .setThumbnail(guild.iconURL())
+  .addFields({name: "Identifiant", value: `${guild.id}`},
+             {name: "Nombre de membres", value: `${guild.memberCount}`},
+             {name: "Description du serveur", value: `${guild.description || "Aucune description"}`},
+             {name: "Date de création", value: `${created}`});
+
+  
+client.channels.cache.get('930361647040720937').send({ embeds: [leaveEmbed]});
+  console.log(`J'ai quitté le serveur ${guild.name}, je suis maintenant dans ${client.guilds.cache.size} serveurs`);
+});
+
 client.login(process.env.DISCORD_TOKEN);
