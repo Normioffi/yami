b9ae77380c1f7d0e14ac255a844f8fba487bc476
Normi offi <88531106+Normioffi@users.noreply.github.com>
2022-02-02 12:09:37 +0100

    Mise à jour serverinfo.js

--- slashcmd/serverinfo.js
+++ slashcmd/serverinfo.js
@@ -5,11 +5,12 @@ module.exports = {
   data: new SlashCommandBuilder()
     .setName("serverinfo")
     .setDescription("Renvoie les informations du serveur"),
-  async execute(interaction, client) {
+  async execute(interaction) {
 
 const dateCreated = moment(interaction.guild.createdAt);
     let serverEmbed = new MessageEmbed()
-    .setColor('#FFFFFF')
+    .setColor('#fcfcc5')
+    .setThumbnail(`${interaction.guild.iconURL() || "https://cdn.glitch.global/6b088ee7-7faa-4199-9d3f-68a689f34c09/discord-crashing~2.jpg"}`)
     .setTitle(interaction.guild.name)
     .setDescription(`**Propriétaire**: <@${interaction.guild.ownerId}>
     
@@ -17,13 +18,30 @@ const dateCreated = moment(interaction.guild.createdAt);
     
     **Membre**: ${interaction.guild.memberCount}
     
-    **Nombre de Salon**: ${interaction.guild.channels.cache.size}
+    **Nombre de Salons**: ${interaction.guild.channels.cache.size}
     
     **Nombre de Rôles**: ${interaction.guild.roles.cache.size}
+    
+    **Nombre de Boost**: ${interaction.guild.premiumSubscriptionCount}
+    
+    **Niveau**: ${interaction.guild.premiumTier}
+    
+    **Description**: ${interaction.guild.description || "Aucune description"}
     
     **Crée le**: ${dateCreated.format("DD/MM/YYYY LTS")}`);
     
     await interaction.reply({ embeds: [serverEmbed] });
-
+    
+    if (interaction.member.permissions.has("ADMINISTRATOR")) {
+    let ephemeralEmbed = new MessageEmbed()
+    .setColor('#fcfcc5')
+    .setTitle("Information supplémentaire")
+    .setDescription(`**Nombre de personnes bannis**: ${interaction.guild.bans.cache.size}
+    
+    **Nombre d'invitation**: ${interaction.guild.invites.cache.size}`);
+      
+      await interaction.followUp({ embeds: [ephemeralEmbed], ephemeral: true})
+  }
+  
   },
 };
